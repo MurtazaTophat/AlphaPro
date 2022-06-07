7864a18f95099157f30c4b35b104710921d438b6
MurtazaTophat <murtaza.tophat@gmail.com>
2022-06-07 16:54:22 +0530

    Product Master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,33 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+
+export interface PeriodicElement {
+  name: string;
+  position: string;
+  weight: string;
+  symbol: string;
+  product: string;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+  {position: 'Product Categories', name: 'Bags & Baskets', weight: '-', symbol: '74427001508', product: '004-Pedrini Poul Try', description:'-'},
+];
 
 @Component({
   selector: 'app-root',
@@ -7,4 +36,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AlphaPro';
+  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol', 'product', 'description'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+  ngOnInit(): void {
+  }
+  public Flag:Boolean=false;
+  public ToggelOpen():void{
+    this.Flag=!this.Flag
+
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
---
+++ src/app/mat/mat.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule } from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatRippleModule} from '@angular/material/core';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatRadioModule} from '@angular/material/radio';
+
+const material = [
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTableModule,
+  MatCardModule,
+  MatButtonToggleModule,
+  MatInputModule,
+  MatDialogModule,
+  MatMenuModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatRippleModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatRadioModule
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    material
+  ],
+  exports: [
+    material
+  ]
+})
+export class MatModule { }
